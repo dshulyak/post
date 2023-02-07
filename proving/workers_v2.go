8919d6a233ce742a87c4376cf8bd83efcc363431
Dmitry <yashulyak@gmail.com>
2023-02-07 15:25:01 +0200

    var length output and pad to 40 bits

--- proving/workers_v2.go
+++ proving/workers_v2.go
@@ -2,8 +2,6 @@ package proving
 
 import (
 	"context"
-	"log"
-	"math"
 
 	"github.com/zeebo/blake3"
 )
@@ -16,21 +14,21 @@ type IndexReporterNew interface {
 	Report(ctx context.Context, nonce uint32, idx uint64) (stop bool)
 }
 
+func le40(b []byte) uint64 {
+	_ = b[4]
+	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+		uint64(b[4])<<32
+}
+
 func workNewBlake(ctx context.Context, data <-chan *batch, reporter IndexReporterNew, ch Challenge, difficulty []byte) {
 	// Params:
 	// Blake's output size in bits
 	const m = 512
-	// dd: |d| = log2(N) - log2(B). Assumed both N and B are power of 2.
-	dd := uint(math.Log2(N) - math.Log2(B))
-	// numOuts = ceil(numNonces * |d| / m )
-	numOuts := uint8(math.Ceil(float64(numNonces*dd) / m))
+	const dsize = 5 // padded to 40 bits
 
-	h, err := blake3.NewKeyed(ch)
-	if err != nil {
-		log.Panicf("Failed to create new blake3 Hasher: %v", err)
-	}
-
-	out := make([]byte, 128)
+	dval := le40(difficulty)
+	h := blake3.New()
+	out := make([]byte, dsize*numNonces)
 
 	for batch := range data {
 		index := batch.Index
@@ -39,28 +37,19 @@ func workNewBlake(ctx context.Context, data <-chan *batch, reporter IndexReporte
 			block := labels[:B]
 			labels = labels[B:]
 
-			for i := uint8(0); i < numOuts; i++ {
-				h.Reset()
-				h.Write(block)
-				h.Write([]byte{i})
-				d := h.Digest()
-				d.Read(out[i*64 : i*64+64])
+			h.Reset()
+			h.Write(ch)
+			h.Write(block)
+			h.Write([]byte{0})
+			d := h.Digest()
+			d.Read(out) // streams variable length output
+
+			for i := 0; i < numNonces; i++ {
+				// padded to 5 bytes to avoid bit arithmetic
+				if le40(out[i*dsize:]) <= dval {
+					reporter.Report(context.TODO(), uint32(i), index)
+				}
 			}
-
-			// bits := bitstream.NewReader(bytes.NewReader(out))
-			// for j := 0; j < numNonces; j++ {
-			// 	_, err := bits.Read(dd)
-			// 	if err != nil {
-			// 		log.Panicf("Failed to read next %d bits of blake3 hash out: %v", dd, err)
-			// 	}
-
-			// 	// if bytes.Compare(outBlock, difficulty) <= 0 {
-			// 	// 	if stop := reporter.Report(ctx, uint32(j), index); stop {
-			// 	// 		batch.Release()
-			// 	// 		return
-			// 	// 	}
-			// 	// }
-			// }
 			index++
 		}
 		batch.Release()
